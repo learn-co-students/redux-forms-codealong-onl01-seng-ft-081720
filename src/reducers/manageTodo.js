b0aa286bb5ea06bbb8b70875a7cf90229a967f6b
Newland-A <ajn252@gmail.com>
2020-12-30 06:53:22 -0500

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,24 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  switch(action.type) {
+    case 'ADD_TODO':
+      console.log({todos:
+      state.todos.concat(action.payload.text) });
 
-  console.log("reducer received this action:", action);
+      return {todos: 
+      state.todos.concat(action.payload.text) };
 
-  return state;
+      default:
+        return state;
+  }
+
+  // console.log("reducer received this action:", action);
+
+  // return state;
 }
+// First, we made sure the React component of our application was working. We did this by building a form, and then making sure that whenever the user typed in the form's input, the state was updated.
+
+// Second, We connected the component to Redux and designed our mapDispatchToProps
+
+// Third, we built our reducer such that it responded to the appropriate event and concatenated the payload into our array of todos.
